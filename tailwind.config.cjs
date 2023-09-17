c472150e77e56eb3edd3714737f66b3c84bee13f
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-09-16 23:20:15 -0500

    Finish basic setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#000000",
+      primary: "#cda715",
+      secondary: "#b6eada",
+      content: "#efedff",
+      space: {
+        1: "#03001c",
+        2: "#303047",
+        3: "#301e67",
+        4: "#5b8fb9",
+      },
+    },
+    fontFamily: {
+      ssp: ["Source Sans Pro", "sans-serif"],
+      audiowide: ["Audiowide", "cursive"],
+    },
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: [
+    "accessibility",
+    "alignItems",
+    "backgroundColor",
+    "borderColor",
+    "display",
+    "flex",
+    "flexBasis",
+    "flexDirection",
+    "flexGrow",
+    "flexShrink",
+    "flexWrap",
+    "fontFamily",
+    "fontSize",
+    "fontWeight",
+    "gap",
+    "height",
+    "justifyContent",
+    "margin",
+    "padding",
+    "textAlign",
+    "textColor",
+    "textTransform",
+    "width",
+  ],
+};
