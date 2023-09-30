7df16bdf7a40161f4289bb6b04dd829c4b7b679d
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-09-30 17:37:05 -0500

    Merge from Master

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
