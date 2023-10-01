92c463ff0d9509dba90da4009307857f8292a1fc
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-10-01 00:15:54 -0500

    More js

--- src/js/photoGallery.js
+++ src/js/photoGallery.js
@@ -6,20 +6,31 @@ document.getElementById("prev")?.addEventListener("click", prevImg);
 const imageButtons = document.querySelectorAll(".image-button");
 const primaryImg = document.querySelector(".gallery-primary-img");
 
-function switchImg(newIndex) {
-  index = newIndex;
+function switchImg(src, alt) {
+  primaryImg.src = src;
+  primaryImg.alt = alt;
 }
 
 function prevImg() {
   if (index > 0) {
     index--;
-    primaryImg.src = this.data[index].src;
+    switchImg(this.data[index].src, this.data[index].alt);
   }
 }
 
 function nextImg() {
   if (index < this.data.length - 1) {
     index++;
-    primaryImg.src = this.data[index].alt;
+    switchImg(this.data[index].src, this.data[index].alt);
   }
 }
+
+imageButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const imgSrcEl = button.querySelector('img').src; 
+    const imgAltEl = button.querySelector('img').alt; 
+
+    switchImg(imgSrcEl, imgAltEl);
+  })
+} 
+
