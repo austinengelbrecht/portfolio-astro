ed8fd6b149ca76a9dfedbb2c639c4b61e5eeca53
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-10-01 02:24:52 -0500

    Update the js to be fully functional

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,4 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue(), mdx()],
-  svg: new URL("./src/assets/svg", import.meta.url).toString(),
 });
